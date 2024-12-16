fbfb73dffd260abec0e2ab62e04b93815bbeb246
gustavo <gustavo.isaac@gmail.com>
2024-12-16 13:15:04 +0000

    viewModelModule

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.plcoding.bookpedia.di
+
+import com.plcoding.bookpedia.book.presentation.booklist.BookListViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModelOf(::BookListViewModel)
+}

d9b27f6a6db7435d0cc2012ff2a263691e306c05
gustavo <gustavo.isaac@gmail.com>
2024-12-16 18:09:37 +0000

    SelectedBookViewModel

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.plcoding.bookpedia.di
 
+import com.plcoding.bookpedia.book.presentation.SelectedBookViewModel
 import com.plcoding.bookpedia.book.presentation.booklist.BookListViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::BookListViewModel)
+    viewModelOf(::SelectedBookViewModel)
 }
